4fc1f1afebfeb0ec31adf1dc902948cbabc579b6
Timothynor <47374517+Timothynor@users.noreply.github.com>
2019-03-04 12:32:08 -0700

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,6 @@
+public class Player{
+    int health;
+    int defence;
+    int controls;
+    String[] weapons = {"rocket", "blade","gun","gadgets"};
+}
---
+++ Weapons.java
@@ -0,0 +1,6 @@
+public class Weapons extends Player{
+    int[] weaponAttributes = { {0, 0, 0,}, {0,0,0}, {0,0,0} };
+    void addNewWeapons(){
+        weapons.add(weaponAttributes);
+    }
+}
